bcc21476b6134671526b6d02f6e59b9a3c8c4995
Yuchen Li <nikkili@bu.edu>
2024-01-24 23:03:29 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -34,13 +43,26 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+    
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
     
     def __repr__(self):
-        return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+        if isinstance(self.p1, Add) and isinstance(self.p2, Add):
+            return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+        elif isinstance(self.p1, Add):
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        elif isinstance(self.p2, Add):
+            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
+        else:
+            return repr(self.p1) + " / " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
     
 
 class Sub:
@@ -50,7 +72,10 @@ class Sub:
 
     def __repr__(self):
         return "( " + repr(self.p1) + " ) - ( " + repr(self.p2) + " )"
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))
